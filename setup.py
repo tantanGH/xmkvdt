3beca78cf12cbcb8f42585cad79ee7161eb5a592
tantanGH <tantangh68@gmail.com>
2023-11-16 18:19:22 +0900

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="xmkvdt",
-    version="0.3.9",
+    version="0.3.10",
     author="tantanGH",
     author_email="tantanGH@github",
     license='MIT',
